f74dc3226dc31986fac8e57c6151bcb8ba6e6f95
tarielTitirashvili <tarieltitirashvili@gmail.com>
2021-09-08 06:11:40 -0700

    request

--- src/API/API.jsx
+++ src/API/API.jsx
@@ -1,11 +1,11 @@
 import * as axios from 'axios'
 
 let instance = axios.create({
-    withCredentials: true,
-    baseURL: 'http://ventis-pay-admin-api-test.eba-ucd32mtn.eu-central-1.elasticbeanstalk.com',
+    baseURL: 'http://ventis-pay-admin-api-test.eba-ucd32mtn.eu-central-1.elasticbeanstalk.com/',
 })
 
 
 export const login = async (email, password, grant_type = "password", store = "VENTIS" )=>{
-    return await instance.post('/api/p/admin/oauth/token', {email, password, grant_type, store })
+    return await instance.post('api/p/admin/oauth/token', `grant_type=${grant_type}&username=${email}&password=${password}&store=${store}`)
 }
+
